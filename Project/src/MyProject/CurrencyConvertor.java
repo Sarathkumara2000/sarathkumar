f1a7040f150e1686dd4eb0ece55daad8c409e978
Sarathkumara2000 <96562869+Sarathkumara2000@users.noreply.github.com>
2022-01-13 05:25:19 -0800

    Add files via upload

---
+++ Project/src/MyProject/CurrencyConvertor.java
@@ -0,0 +1,18 @@
+package MyProject;
+
+import java.util.Scanner;
+
+public class CurrencyConvertor {
+
+	public static void main(String[] args) {
+		double currency;
+		Scanner sc=new Scanner (System.in);
+		System.out.println("ente the currency:");
+currency =sc.nextDouble();
+while(currency>0){
+	
+}
+
+	}
+
+}
